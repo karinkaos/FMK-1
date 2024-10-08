d9f4d4dd9dd4f563b8349e1997dbc4d166b69374
Christoffer Lundberg <148043316+Obaoba90@users.noreply.github.com>
2024-10-08 14:41:01 +0200

    Change color buttons

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,13 +1,25 @@
 using System;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 
 namespace FMK_1
 {
     public sealed partial class MainPage : Page
     {
-        private Random random = new Random();
+        //Color List
+		private Color[] colors = new Color[]
+		{
+			Colors.Red,    
+            Colors.Green, 
+            Colors.Blue,   
+            Colors.Yellow 
+        };
+
+		private int colorIndex = 0;  //Used to go through Color List.
+		private Random random = new Random();
 
         public MainPage()
         {
@@ -71,5 +83,19 @@ namespace FMK_1
             Dot5.Visibility = Visibility.Collapsed;
             Dot6.Visibility = Visibility.Collapsed;
         }
+
+		private void ChooseClr_Click(object sender, RoutedEventArgs e)
+		{
+            Button clickedButton = sender as Button;
+			            
+			clickedButton.Background = new SolidColorBrush(colors[colorIndex]);
+			
+			colorIndex++;
+
+			if (colorIndex >= colors.Length)
+			{
+				colorIndex = 0;
+			}
+		}
     }
 }
